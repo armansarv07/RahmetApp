c9a68d5ca529c0cb0c99caeca1937c4e903d2ed3
Elvina Shamoi <elvinashm@gmail.com>
2022-02-07 16:02:24 +0600

    first commitment

---
+++ Rahmet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rahmet
+//
+//  Created by Elvina Shamoi on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
