93523f669738ab9056cee8fcdddacade0cea33c3
Arman Sarvadin <78083072+armansarv07@users.noreply.github.com>
2022-02-12 16:33:33 +0600

    Fixed conflict

--- Rahmet/View/TabBarViews/MainMenu.swift
+++ Rahmet/View/TabBarViews/MainMenu.swift
@@ -4,37 +4,69 @@
 //
 //  Created by Arman on 09.02.2022.
 //
-
 import UIKit
-
+import SnapKit
 
 class MainMenu: UIViewController {
-    var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .secondarySystemBackground
         setupNavigationBar()
+        setupViews()
+        setupConstraints()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    var fakeData: [Cafe] = [
+        Cafe(name: "Del Papa", address: "ул. Бухар жырау, 66, уг. ул. Ауэзова", imgName: "cafeImage"),
+        Cafe(name: "Del Papa2", address: "ул. Бухар жырау, 66, уг. ул. Ауэзова", imgName: "cafeImage"),
+        Cafe(name: "Del Papa3", address: "ул. Бухар жырау, 66, уг. ул. Ауэзова", imgName: "cafeImage"),
+        Cafe(name: "Del Papa4", address: "ул. Бухар жырау, 66, уг. ул. Ауэзова", imgName: "cafeImage"),
+        Cafe(name: "Del Papa5", address: "ул. Бухар жырау, 66, уг. ул. Ауэзова", imgName: "cafeImage"),
+        Cafe(name: "Del Papa6", address: "ул. Бухар жырау, 66, уг. ул. Ауэзова", imgName: "cafeImage")
+    ]
+    
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = true
+        return tableView
+    }()
 }
 
 
 extension MainMenu: LayoutForNavigationVC {
     func setupConstraints() {
-        
+        tableView.snp.makeConstraints { make in
+            make.left.right.top.bottom.equalToSuperview()
+        }
     }
     
     func setupNavigationBar() {
-        navigationItem.title = "Мои заказы"
+        navigationItem.title = "Заказ с собой"
     }
     
-    func setupCollectionView() {
-        
+    func setupViews() {
+        view.addSubview(tableView)
+        tableView.register(CafeCell.self, forCellReuseIdentifier: "cafeCell")
+    }
+}
+
+extension MainMenu: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fakeData.count
     }
     
-//    func createCompostionalLayout() -> UICollectionViewCompositionalLayout {
-//
-//    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cafeCell", for: indexPath) as! CafeCell
+        cell.cafe = fakeData[indexPath.row]
+        return cell
+    }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+       return 120
+    }
 }
 
 
